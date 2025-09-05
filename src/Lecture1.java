43d2b45c7f287c8817e88d748ccc0c1b3aab9003
TOMAR-PARAKRAM <tomarparakram17@gmail.com>
2025-09-05 22:16:30 +0530

    memory management

--- src/Lecture1.java
+++ src/Lecture1.java
@@ -1,5 +1,25 @@
 // so we are learning types of memory and the memory management
 
 //so there are 3 types of language
-1} procedural => they follow the particular systmatic or the flow of the execution.
-2} functional =>
+//1} procedural => they follow the particular systmatic or the flow of the execution.
+//2} functional => does not mess with existing code , not modifying the original data, we work on the copy of that data
+// 3} object oriented => code+dataa = object
+
+// STATIC V/S DYANAMIC
+//static=> a}perform type check at compile time
+//         b} error will show at th compile time
+//         c} declare datatype before using it
+//         d} more control
+//
+//
+//dynamic=> a}perform type check at runtime , matlab sab kuch ho gaya ,   jab run hota hai tab check karinge
+//          b} error will show at th compile time
+//          c} no need to declre datatype
+
+
+//memory management
+//consider int a = 10
+//1} stack memory => a(_referencevariable )
+//2} heap memory => 10 (_object)
+
+
