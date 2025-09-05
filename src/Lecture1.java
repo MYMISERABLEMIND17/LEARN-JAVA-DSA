ab2fd8e2639d0ccf3c348c2a323999d2441d422e
TOMAR-PARAKRAM <tomarparakram17@gmail.com>
2025-09-05 18:40:55 +0530

    basically i started setup of java

--- src/Lecture1.java
+++ src/Lecture1.java
@@ -1,1 +1,5 @@
 // so we are learning types of memory and the memory management
+
+//so there are 3 types of language
+1} procedural => they follow the particular systmatic or the flow of the execution.
+2} functional =>
