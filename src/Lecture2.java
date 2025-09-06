9fb91dda5217c9f3e715a76f305595cd45a05f6b
TOMAR-PARAKRAM <tomarparakram17@gmail.com>
2025-09-06 11:58:52 +0530

    completing the lecture 2

--- src/Lecture2.java
+++ src/Lecture2.java
@@ -1,6 +1,23 @@
-public class Lecture2 {
-    public static void main(String[] args) {
-        System.out.println("Hello from Lecture2!");
-    }
-}
+//public class Lecture2 {
+//    public static void main(String[] args) {
+//        System.out.println("Hello from Lecture2!");
+//    }
+//}
+
+// come directly to psudocode
+//==> a rough code
+//Q1 => input a number and print whether it is prime or not
+//so the psudocode will be
+//
+//start
+//take a input
+//let c = 2
+//while c<num
+//    if(num%c = 0){
+//        output =not prime
+//        }
+// exit
+
+// there is something called sqrt method , make sure aapko aata hai
+
 
