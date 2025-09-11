df4f6d476ca437c8b4d1f4eb9e697ddf985763a5
TOMAR-PARAKRAM <tomarparakram17@gmail.com>
2025-09-11 11:21:38 +0530

    random commit

--- src/Lecture3.java
+++ src/Lecture3.java
@@ -1,8 +1,8 @@
-//public class Lecture3 {
-//    public static void main(String[] args){
-//        System.out.println("hello world ");
-//    }
-//}
+public class Lecture3 {
+    public static void main(String[] args){
+        System.out.println("hello world ");
+    }
+}
 
 //In this section we will study how java code executes
 // 1} => .java file => this is
