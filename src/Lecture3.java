94b09819e6b44a04254abebc434d92c49e9b5efd
TOMAR-PARAKRAM <tomarparakram17@gmail.com>
2025-09-11 14:06:16 +0530

    complete

--- src/Lecture3.java
+++ src/Lecture3.java
@@ -1,12 +1,28 @@
-public class Lecture3 {
-    public static void main(String[] args){
-        System.out.println("hello world ");
-    }
-}
+//public class Lecture3 {
+//    public static void main(String[] args){
+//        System.out.println("hello world ");
+//    }
+//}
 
 //In this section we will study how java code executes
-// 1} => .java file => this is
+// 1} => .javafile => this is a simple java file (source code)
+// 2} => .classfile => after compiling the .javafile , we get the .classfile which has {byte code in it(not human redable)}
+// 3} => machine code => interperter will give the machine code 0 and 1 , from .class file
 
+//PLATFORM INDEPENDENCE
+//1} byte code will run on all the Operating system
+//but ......
+//java virtual machine is platform dependend , different for different machine or os
+
+
+//JDK => jre +development tools
+//JRE  =>jvm + library classes
+//JVM => java virtual machine
+
+
+//JDK => a package , a set of files ,
+//javac = compiler
+//and lots of other things
 
 
 
