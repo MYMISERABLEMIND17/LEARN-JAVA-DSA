faf0e742a64f6f968b44367d0c97eefc17eae241
TOMAR-PARAKRAM <tomarparakram17@gmail.com>
2025-09-11 16:48:07 +0530

    break

---
+++ src/Lecture4/input.java
@@ -0,0 +1,25 @@
+package Lecture4;
+
+import java.util.Scanner;
+
+public class input {
+
+    public static void main(String[] args) {
+
+//        Scanner input = new Scanner(System.in) ;
+//        String name = input.toString();
+//        int score = input.nextInt();//ignore , try something but fail
+//        System.out.println(name) ;
+//        System.out.println(score) ;//ignore , try something but fail
+//        System.out.println(name+ " = "+ score ); //ignore , try something but fail
+
+        Scanner input  =  new Scanner(System.in);
+
+        System.out.print("enter your marks = ");
+        int score = input.nextInt();
+        System.out.println("your marks = " + score);
+
+
+
+    }
+}
