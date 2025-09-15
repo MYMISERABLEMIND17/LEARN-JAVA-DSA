38b95ccb53e8259665ff3962773f841c90bce8cf
TOMAR-PARAKRAM <tomarparakram17@gmail.com>
2025-09-15 16:23:50 +0530

    complete the fibonacci series

--- src/Lecture5/CaseCheck.java
+++ src/Lecture5/CaseCheck.java
@@ -1,3 +1,5 @@
+package Lecture5;
+
 import java.util.Scanner;
 
 public class CaseCheck {
--- src/Lecture5/fibonacci.java
+++ src/Lecture5/fibonacci.java
@@ -1,7 +1,23 @@
 package Lecture5;
 
+import java.util.Scanner;
+
 public class fibonacci {
     public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in );
+        int user=  inp.nextInt() ;
 
+        int a = 0 ;
+        int b = 1 ;
+        int count = 2 ;
+
+        while (count <= user ){
+            int temp = b ;
+            b = b + a ;
+            a = temp ;
+            count++ ;
+        }
+
+        System.out.println( b );
     }
 }
