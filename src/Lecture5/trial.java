d6f7c406a68ad72b30b9001b236535f83259e129
TOMAR-PARAKRAM <tomarparakram17@gmail.com>
2025-09-15 14:14:22 +0530

    trying to build the calculator

---
+++ src/Lecture5/trial.java
@@ -0,0 +1,10 @@
+package Lecture5;
+
+public class trial {
+
+    public static void main(String[] args) {
+        System.out.println("hello world ");
+    }
+
+
+}
---
+++ src/PRACTICEWITHPROJECTS/calculator.java
@@ -0,0 +1,32 @@
+// PSUDO CODE
+// step 1 => take the input from the user
+// step 2 => select the operation from what user has given
+// step3 => perform operation
+// STEP 4 =>
+package PRACTICEWITHPROJECTS;
+
+import java.util.Scanner;
+
+public class calculator {
+    public static void main(String[] args) {
+
+
+
+
+        Scanner input = new Scanner(System.in);
+        System.out.println(" enter the first number ");
+        int first = input.nextInt();
+        System.out.println(" enter the first number ");
+        int second = input.nextInt();
+
+        // select the operation
+        String operation1 = input.nextLine();
+        String operation2 = input.nextLine();
+        String operation3 = input.nextLine();
+        String operation4 = input.nextLine();
+
+
+
+
+    }
+}
