370765dfbc703cc2e6c88cc3a3cb878c4ebf76f9
TOMAR-PARAKRAM <tomarparakram17@gmail.com>
2025-09-15 15:54:03 +0530

    just complete the largest number program and loops

---
+++ src/Lecture5/fibonacci.java
@@ -0,0 +1,7 @@
+package Lecture5;
+
+public class fibonacci {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Lecture5/loops.java
@@ -0,0 +1,54 @@
+package Lecture5;
+
+import java.util.Scanner;
+
+public class loops {
+    public static void main(String[] args) {
+//        // print  1 to 5
+        // for loop
+//        for (int x = 1 ; x<=5 ; x++ ){
+//            System.out.println(x);
+//        }
+
+        // while loop
+//        int x = 1 ;
+//        while(x<= 5 ){
+//            System.out.println(x);
+//            x++ ;
+//        }
+
+
+//        number from one to n
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("ENTER THE NUMBER = ");
+        int userinp = input.nextInt() ;
+
+//        for (int x = 0 ; x<=userinp ; x++){
+//            System.out.println(x);
+//        }
+
+
+        //DO WHILE LOOP
+//        int x = 1 ;
+//        do {
+//            System.out.println(x);
+//            x++ ;
+//        }
+//        while(x<=userinp);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
