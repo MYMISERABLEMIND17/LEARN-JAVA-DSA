965d8b6e172c53e47368ab0ee8176e65707cac7c
TOMAR-PARAKRAM <tomarparakram17@gmail.com>
2025-09-19 14:18:25 +0530

    learning about swap

---
+++ src/Lecture7_FunctionAndMethods/swap.java
@@ -0,0 +1,17 @@
+package Lecture7_FunctionAndMethods;
+
+public class swap {
+    public static void main(String[] args) {
+            int a = 20 ;
+            int b = 50 ;
+
+            int temp = a ;
+            a = b ;
+            temp = b ;
+
+        System.out.println(a);
+        System.out.println(b);
+
+
+    }
+}
