2bc20ad3cb141a190110325b06c87b350e37bd0a
TOMAR-PARAKRAM <tomarparakram17@gmail.com>
2025-09-20 12:22:06 +0530

    overloading

---
+++ src/Lecture7_FunctionAndMethods/Overloading.java
@@ -0,0 +1,18 @@
+package Lecture7_FunctionAndMethods;
+
+public class Overloading {
+    public static void main(String[] args) {
+        fun(99);
+        fun("   parakram");
+        // same function but the value that we are passing was dfferent or you can say the datatypes are different `
+    }
+    static void fun(int x  ){
+        System.out.println( " first function " + x );
+    }
+// 2 functions but with different datatypes
+
+    static void fun(String name ){
+
+        System.out.println("second function" + name);
+    }
+}
