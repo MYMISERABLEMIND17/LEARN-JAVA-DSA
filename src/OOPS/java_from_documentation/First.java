0801a43ec48c36c760e788ad9a90b3d97f65704c
TOMAR-PARAKRAM <tomarparakram17@gmail.com>
2025-09-24 14:06:52 +0530

    reading the docs of the java ( java-t-point )

---
+++ src/OOPS/java_from_documentation/First.java
@@ -0,0 +1,15 @@
+package OOPS.java_from_documentation;
+
+public class First {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+class Human{
+    int eye = 2 ;
+    int hands = 2 ;
+    int fingers = 10 ;
+}
+
