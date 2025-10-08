1fb6c175d49b06f960e50f9e4a3fcf0a6a8fdea7
TOMAR-PARAKRAM <tomarparakram17@gmail.com>
2025-10-08 14:17:50 +0530

    x

--- src/Arrays/lect1/MultiDimensional.java
+++ src/Arrays/lect1/MultiDimensional.java
@@ -1,11 +1,24 @@
 package Arrays.lect1;
 
+import java.util.Arrays;
+
 public class MultiDimensional {
     public static void main(String[] args) {
         /*
             1 2 3
             4 5 6
             7 8 9
-             */
+         */
+
+//        int[][] arr = new int["number of rows "]["number of columns"]
+        int[][] array= new int[3][3] ;
+        int[][] array2d = {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        };
+
+        System.out.println(array2d[1]);
+
     }
 }
