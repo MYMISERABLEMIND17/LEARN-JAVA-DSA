c9f59c33d3cccd2d9e18934879e89c3ec9456eea
TOMAR-PARAKRAM <tomarparakram17@gmail.com>
2025-10-09 11:38:08 +0530

    learning Array list  for infinite input

---
+++ src/Arrays/lect1/ArrayListlearn.java
@@ -0,0 +1,55 @@
+package Arrays.lect1;
+
+import java.util.ArrayList;
+
+public class ArrayListlearn {
+    public static void main(String[] args) {
+        // syntax = ArrayListlearn<Integer> name = new ArrayListlearn<>() ;
+
+
+        ArrayList<Integer> list = new ArrayList<>(10) ;
+        list.add(100) ;
+        list.add(100) ;
+        list.add(100) ;
+        list.add(50) ;
+        list.add(100) ;
+        list.add(100) ;
+        list.add(100) ;
+        list.add(100) ;
+        list.add(100) ;
+        list.add(100) ;
+        list.add(10000) ;
+
+        System.out.println(list);
+
+
+//        updation
+        list.set(3 ,55) ;
+        System.out.println(list);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
