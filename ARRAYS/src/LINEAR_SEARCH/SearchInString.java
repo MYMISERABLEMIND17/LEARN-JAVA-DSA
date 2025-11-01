9646d5d850477b454817eb7e68c9f95d33d1794a
MYMISERABLEMIND17 <tomarparakram17@gmail.com>
2025-11-01 14:19:38 +0530

    complete linear search in string

---
+++ ARRAYS/src/LINEAR_SEARCH/SearchInString.java
@@ -0,0 +1,22 @@
+package LINEAR_SEARCH;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SearchInString {
+    public static void main(String[] args) {
+
+        // take input from the user
+        Scanner inp = new Scanner(System.in) ;
+        System.out.print("enter the string = ");
+        String type = inp.next() ;
+        System.out.print("which character do you want to find = ");
+        char target = inp.next().charAt(0) ;
+
+        for (int j = 0; j <type.length()-1 ; j++) {
+            if(target == type.charAt(j)){
+                System.out.println("character found at index " + j);
+            };
+        }
+    }
+}
