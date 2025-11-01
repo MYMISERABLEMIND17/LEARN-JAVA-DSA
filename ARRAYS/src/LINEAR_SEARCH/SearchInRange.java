a799319eb252685dc84811f8a3e40e1254b16729
MYMISERABLEMIND17 <tomarparakram17@gmail.com>
2025-11-01 15:30:28 +0530

    complete

---
+++ ARRAYS/src/LINEAR_SEARCH/SearchInRange.java
@@ -0,0 +1,43 @@
+package LINEAR_SEARCH;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SearchInRange {
+    public static void main(String[] args) {
+        // take input array from user
+
+        Scanner inp = new Scanner(System.in) ;
+        System.out.print("how many elements you want in array = ");
+        int element = inp.nextInt() ;
+
+        int[] MyArr = new int[element] ;
+        for (int k = 0; k < element; k++) {
+            MyArr[k] = inp.nextInt();
+        }
+        System.out.println(Arrays.toString(MyArr));
+
+        System.out.print("initial index = ");
+        int i = inp.nextInt();
+        System.out.print("final index = ");
+        int j = inp.nextInt();
+        System.out.print("target value = ");
+        int target = inp.nextInt();
+
+
+
+
+        loop(MyArr, i ,j, target);
+    }
+
+
+
+    public static void loop (int Arr[] , int start , int end, int val ){
+        while(start<=end){
+            if (val == Arr[start]){
+                System.out.println("value found at "+ start + " index");
+            }
+            start++ ;
+        }
+    }
+}
