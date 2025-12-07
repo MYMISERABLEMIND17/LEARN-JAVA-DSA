feee62131cebe69cdb41ab371d41caa1f72d73dd
MYMISERABLEMIND17 <tomarparakram17@gmail.com>
2025-12-07 22:34:22 +0530

    Initial commit

--- ARRAYS/src/SELECTION_SORT/Selection_sort.java
+++ ARRAYS/src/SELECTION_SORT/Selection_sort.java
@@ -1,4 +1,12 @@
 package SELECTION_SORT;
 
 public class Selection_sort {
+    public static void main(String[] args) {
+        int[] arr ={2,5,3,6,7} ;
+    }
+
+    public static void selection (int[] Myarr ){
+        for
+    }
+
 }
