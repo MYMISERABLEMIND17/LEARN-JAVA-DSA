63a80f118a5af28e9cbb7b9570cdf54bd9213e3f
MYMISERABLEMIND17 <tomarparakram17@gmail.com>
2025-12-15 16:35:50 +0530

    complete the selection sort on my own

--- ARRAYS/src/SELECTION_SORT/Selection.java
+++ ARRAYS/src/SELECTION_SORT/Selection.java
@@ -2,34 +2,49 @@ package SELECTION_SORT;
 
 import java.util.Arrays;
 
-public class Selection {
-    public static void main(String[] args) {
-        int[] myarr = {4,8,6,2,7,1,5} ;
-
-        // find the maximum element
-
-        int min = myarr[0] ;
-        int x = 0;
-
-        for(int j = 0 ; j<= myarr.length-1; j++){
-            for (int i = j ; i<= myarr.length-1; i++ ){
-                if( min > myarr[i]){
-                    min = myarr[i];
-                }
+        public class Selection {
+            public static void main(String[] args) {
+                int[] myarr = {1,5,-3,2,4} ;
+                selection(myarr);
 
             }
-            for (int k = j; k <= myarr.length-1 ; k++) {
-                if(min > myarr[k] ){
-//                    swap
-                    min = x ;
-                    x = myarr[k] ;
-                    myarr[k] = min ;
+
+            static void selection(int[] arr){
+                int end = arr.length-1 ;
+                for(int i= 0; i<= arr.length-1 ; i++){
+                     end = arr.length-1-i ;
+
+
+
+                    // find max element
+                    int max = 0 ;
+                    for(int j = 1 ;j <= end ; j++){
+                        if(arr[j] >  arr[max] ){
+                            max = j;
+                        }
+                    }
+
+                    // swap
+
+                    int temp = arr[max] ;
+                    arr[max] = arr[end];
+                    arr[end] = temp ;
+
+
 
                 }
+                System.out.println(Arrays.toString(arr));
+
+
             }
+
+
+
+
+
+
+
+
         }
 
 
-
-    }
-}
