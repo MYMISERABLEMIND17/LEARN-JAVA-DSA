b4b8ad2d5f2c0387a24bf6d305cce6eddb4da770
MYMISERABLEMIND17 <tomarparakram17@gmail.com>
2025-12-17 18:54:01 +0530

    solved problem 1

---
+++ QUESTION SOLVING/src/Problem_1.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Problem_1 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.print("ENTER THE final NUMBERS =");
+        int target = inp.nextInt();
+        int[] nums = {2, 3, 4, 6, 8, 10};
+
+        for (int i = 0; i <= nums.length - 1; i++) {
+            for (int j = i+1; j <= nums.length - 1; j++) {
+                if (nums[i] + nums[j] == target) {
+                    int[] out = new int[2];
+                    out[0] = i;
+                    out[1] = j;
+                    System.out.println(Arrays.toString(out));
+                }
+            }
+        }
+
+
+    }
+}
