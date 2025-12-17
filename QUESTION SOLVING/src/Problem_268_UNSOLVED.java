8f45dcfa2faf0f524b8ec67fd1f4c3cd8ebf2d7f
MYMISERABLEMIND17 <tomarparakram17@gmail.com>
2025-12-17 17:15:33 +0530

    solved 374

---
+++ QUESTION SOLVING/src/Problem_268_UNSOLVED.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Problem_268_UNSOLVED {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in );
+        System.out.print("ENTER THE TOTAL NUMBERS = ");
+        int elem = inp.nextInt() ;
+        int[] myarr = new int[elem];
+
+        input(myarr ,elem );
+        swaplogic(myarr);
+
+    }
+    static void input(int[] arr , int elem ) {
+        Scanner inp = new Scanner(System.in);
+        for (int i = 0; i <= elem - 1; i++) {
+            arr[i] = inp.nextInt();
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void swaplogic(int[] arr) {
+        // first sort the array
+        int i = 0;
+        while (i <= arr.length - 1) {
+            if (arr[i] == i) {
+                i++;
+            } else if (arr[i] != i) {
+                // swap
+                int temp = arr[i];
+                arr[i] = i;
+                i = temp;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+
+
+
+
+}
---
+++ QUESTION SOLVING/src/Problem_374.java
@@ -0,0 +1,24 @@
+
+public class Problem_374 {
+
+    public int guessNumber(int n) {
+        int start = 1;
+        int end = n;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            int res = guess(mid);
+
+            if (res == 0) {
+                return mid;          // correct guess
+            } else if (res < 0) {
+                end = mid - 1;       // guess is too high
+            } else {
+                start = mid + 1;     // guess is too low
+            }
+        }
+        return -1; // never reached
+    }
+}
+
+
