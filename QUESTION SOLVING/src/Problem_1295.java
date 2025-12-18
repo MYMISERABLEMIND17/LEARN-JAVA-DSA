11bb5e2d71429c250142b1667713467873d36486
MYMISERABLEMIND17 <tomarparakram17@gmail.com>
2025-12-18 12:34:21 +0530

    complete the problem 1295

---
+++ QUESTION SOLVING/src/Problem_1295.java
@@ -0,0 +1,22 @@
+public class Problem_1295 {
+    public static void main(String[] args) {
+        int[] arr ={555,901,482,1771} ;
+        int count = 0 ;
+        for (int i=0 ; i<arr.length ; i++){
+            int digit = 0 ;
+            int n = arr[i];
+            while( n != 0   ){
+                 n = n/10 ;
+                 digit++ ;
+            }
+            if(digit%2 == 0){
+                count++ ;
+            }
+
+        }
+        System.out.println(count);
+
+
+
+    }
+}
