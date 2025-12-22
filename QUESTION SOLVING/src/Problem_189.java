eafb2081108a352595fdcf06a54abb48f6b2acfe
MYMISERABLEMIND17 <tomarparakram17@gmail.com>
2025-12-22 09:55:57 +0530

    solve problem 189 but leetcode wants optimized approach

---
+++ QUESTION SOLVING/src/Problem_189.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class Problem_189 {
+    public static void main(String[] args) {
+        int[]  arr = {1,2,3,4,5,6,7 } ;
+        int n = 2 ;
+        rotate(arr , n );
+    }
+    static void rotate(int[] nums , int k  ){
+            for(int i =0 ; i<k ;i++ ){
+                int temp = nums[nums.length -1 ] ;
+                for (int j  = nums.length-1; j >0  ; j--) {
+                    // time to swap
+                    nums[j] = nums[j-1];
+                }
+                nums[0] = temp ;
+
+            }
+            System.out.println(Arrays.toString(nums));
+    }
+
+}
+
