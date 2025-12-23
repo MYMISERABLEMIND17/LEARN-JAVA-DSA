7da97f653a0686f48c285489ae4ab154900a58d7
MYMISERABLEMIND17 <tomarparakram17@gmail.com>
2025-12-23 10:40:10 +0530

    solved the question 2592 , but didnt completly solve 73

---
+++ QUESTION SOLVING/src/Problem_2529.java
@@ -0,0 +1,19 @@
+public class Problem_2529 {
+    public static void main(String[] args) {
+        int[] arr = {5,20,66,1314} ;
+        logic(arr) ;
+        System.out.println(logic(arr));
+    }
+    static int logic(int[] nums){
+        int neg = 0 ;
+        int pos = 0 ;
+        for (int i = 0; i < nums.length ; i++) {
+            if(nums[i] < 0  ){
+                neg++ ;
+            } else if (nums[i] >0) {
+                pos++ ;
+            }
+        }
+        return Math.max(pos , neg) ;
+    }
+}
---
+++ QUESTION SOLVING/src/Problem_73.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class Problem_73 {
+    public static void main(String[] args) {
+        int[][] arr = {{1,1,1} ,{1,0,1} , {1,1,1}} ;
+        logic(arr);
+    }
+    static void logic (int[][] nums ){
+
+        System.out.println(nums.length);
+        for (int i = 0; i < nums.length ; i++) {
+            for (int j = 0; j < nums[i].length ; j++) {
+                if(nums[i][j] == 0){
+                    System.out.println(i +""+ j);
+                    int k = 0 ;
+                    while (k<nums[i].length){
+                        nums[k][j] = 0 ;
+                        k++ ;
+                    }
+                }
+            }
+        }
+        System.out.println(Arrays.deepToString(nums));
+    }
+}
