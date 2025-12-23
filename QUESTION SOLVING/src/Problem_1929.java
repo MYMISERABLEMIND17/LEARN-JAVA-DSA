d51a1297dafebd7e434d9478ef29be2b91376f2f
MYMISERABLEMIND17 <tomarparakram17@gmail.com>
2025-12-23 09:15:04 +0530

    complete the question 1929 and its really easy questions

---
+++ QUESTION SOLVING/src/Problem_1929.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class Problem_1929 {
+    public static void main(String[] args) {
+        int[] nums = {3, 4, 2, 1};
+        concat(nums);
+    }
+
+    static void concat(int[] nums) {
+        int[] out = new int[nums.length * 2];
+//    System.out.println(nums.length*2);
+        int n = nums.length ;
+        for (int i = 0; i < nums.length ; i++) {
+            out[i] = nums[i] ;
+            out[i+n] = nums[i] ;
+        }
+        System.out.println(Arrays.toString(out)) ;
+    }
+
+
+}
---
+++ QUESTION SOLVING/src/Problem_349_UNSOLVED.java
@@ -0,0 +1,19 @@
+//public class Problem_349_UNSOLVED {
+//    public static void main(String[] args) {
+//        int[] arr1 = {} ;
+//        int[] arr2 = {} ;
+//        int[] out = {} ;
+//        intersection(arr1 ,arr2, out) ;
+//    }
+//
+//    static void intersection(int[]arr1, int[] arr2 , int[] out){
+//        for (int i = 0; i <arr1.length ; i++) {
+//            for (int j = i; j < arr2.length; j++) {
+//                if (arr1[i] == arr2[j]){
+//
+//                }
+//            }
+//        }
+//    }
+//
+//}
