11de0bd65c054a24f1fd05158bb96db8ed90df71
MYMISERABLEMIND17 <tomarparakram17@gmail.com>
2025-12-24 11:55:54 +0530

    complete the problem 3074

---
+++ QUESTION SOLVING/src/Problem_3074.java
@@ -0,0 +1,52 @@
+import java.util.Arrays;
+
+public class Problem_3074 {
+    public static void main(String[] args) {
+        int[] apples = {9,8,8,2,3,1,6} ;
+        int[] boxes = {10,1,4,10,8,5} ;
+
+        System.out.println(logic(apples , boxes));
+    }
+    static int  logic( int[] apple , int[] capacity){
+        // sort the capacity
+
+        for (int i = 0; i <capacity.length ; i++) {
+            int min = i ;
+            for (int j = i+1 ; j <capacity.length ; j++) {
+                if(capacity[j] > capacity[min]){
+                    min = j ;
+                }
+            }
+            //swap
+            int temp = capacity[i] ;
+            capacity[i] = capacity[min] ;
+            capacity[min] = temp ;
+        }
+        System.out.println(Arrays.toString(capacity));
+        // now find the total apple
+
+        int total= 0 ;
+        int h = 0 ;
+        while( h < apple.length){
+            total = total + apple[h] ;
+            h++ ;
+        }
+        System.out.println(total);
+
+        // on more loop in which we will compare capacity with total , create a count variable
+        int sum = 0 ;
+        int  count = 0 ;
+        int t= 0 ;
+        while(sum <= total && t < capacity.length ){
+            sum = sum + capacity[t];
+            count ++ ;
+            t++ ;
+            if(sum ==total){
+                break ;
+            }
+        }
+        
+        return count ;
+
+    }
+}
