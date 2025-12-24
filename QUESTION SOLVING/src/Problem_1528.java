37d26088c479283eaf5440f5883fc851db24dad3
MYMISERABLEMIND17 <tomarparakram17@gmail.com>
2025-12-24 13:06:19 +0530

    complete the problem 1528

---
+++ QUESTION SOLVING/src/Problem_1528.java
@@ -0,0 +1,20 @@
+public class Problem_1528 {
+    public static void main(String[] args) {
+        String s = "codeleet" ;
+        int[] index = {4,5,6,7,0,2,1,3} ;
+
+        System.out.println(logic(s , index));
+    }
+    static String logic(String s , int[] indices){
+
+        char[] result = new char[s.length()];
+
+
+        for (int i = 0; i < s.length(); i++) {
+            result[indices[i]] = s.charAt(i);
+        }
+
+
+        return new String(result);
+    }
+}
