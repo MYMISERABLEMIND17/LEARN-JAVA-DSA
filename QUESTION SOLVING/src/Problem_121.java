5f5c591ede8157cc38c44ca70f7d2a3c1ca1975c
MYMISERABLEMIND17 <tomarparakram17@gmail.com>
2025-12-29 14:07:55 +0530

    solve the problem but there is an error bcoz i solve this with n2 complexity  , so now i will find better solution

--- QUESTION SOLVING/src/Problem_121.java
+++ QUESTION SOLVING/src/Problem_121.java
@@ -2,29 +2,24 @@ import java.util.Arrays;
 
 public class Problem_121 {
     public static void main(String[] args) {
-        int[] arr = {7,1,5,3,6,4} ;
+        int[] arr = {7,6,4,3,1} ;
 
         System.out.println(logic(arr));
     }
 
-    static int logic(int[] arr) {
-        for (int i = 0; i <arr.length ; i++) {
-            int min = i ;
-            for (int j = i+1; j < arr.length ; j++) {
-                if(arr[j] < arr[min]){
-                    min = j ;
+    static int logic(int[] prices) {
+        int maxprofits = 0 ;
+        for (int i = 0; i < prices.length; i++) {
+            int profit = 0 ;
+            for (int j = i+1; j < prices.length ; j++) {
+                profit = prices[j] - prices[i] ;
+                if(profit<0){
+                    break ;
+                } else if (profit > maxprofits) {
+                    maxprofits = profit ;
                 }
             }
-            int temp = arr[min] ;
-            arr[min] = arr[i] ;
-            arr[i] = temp ;
         }
-
-
-        int maxprofit = arr[arr.length-1] - arr[0] ;
-        if(maxprofit > 0 ){
-            return maxprofit ;
-        }
-        return 0 ;
+        return maxprofits ;
     }
 }
