0cb47fdca4f0e9982d16c15e2f3bbbf843a81e76
MYMISERABLEMIND17 <tomarparakram17@gmail.com>
2025-12-30 11:52:10 +0530

    solved the problem 35

---
+++ QUESTION SOLVING/src/Problem_35.java
@@ -0,0 +1,23 @@
+public class Problem_35 {
+    public static void main(String[] args) {
+        int[] arr = {1,3,5,6} ;
+        int target = 7 ;
+        System.out.print(logic(arr , target));
+    }
+
+    static int logic(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target == nums[mid]) {
+                return mid;
+            } else if (target > nums[mid]) {
+                start = mid + 1;
+            } else if (target < nums[mid]) {
+                end = mid - 1;
+            }
+        }
+        return start ;
+    }
+}
--- QUESTION SOLVING/src/SOLVED/Problem_704.java
+++ QUESTION SOLVING/src/SOLVED/Problem_704.java
@@ -1,3 +1,5 @@
+package SOLVED;
+
 public class Problem_704 {
     public static void main(String[] args) {
         int[] arr = {1,5,8,9,10} ;
