ee55c6d4899288535479c945a1483737db020708
MYMISERABLEMIND17 <tomarparakram17@gmail.com>
2026-01-02 14:38:34 +0530

    solved problem 268

--- QUESTION SOLVING/src/UNSOLVED/Problem_268_UNSOLVED.java
+++ QUESTION SOLVING/src/UNSOLVED/Problem_268_UNSOLVED.java
@@ -2,23 +2,20 @@ package UNSOLVED;
 
 public class Problem_268_UNSOLVED {
     public static void main(String[] args) {
-        int[] arr = {0,1} ;
-
-        System.out.println(logic(arr));
+        int[] arr = {0, 1 } ;
+        logic(arr);
     }
-    static int  logic(int[] nums) {
-        int x = 0 ;
-        for (int i = 0; i <nums.length ; i++) {
-            for (int j = 0; j < nums.length ; j++) {
-                if(i == nums[j]){
-                    break;
-                }
-                else if (i != nums[j]) {
-                    x = i ;
-                }
-
-            }
+    static void  logic(int[] nums) {
+        int arrsum = nums.length;
+        int valsum = 0 ;
+        for (int i = 0; i <= nums.length-1 ; i++) {
+            arrsum =  arrsum + i ;
+            valsum = valsum + nums[i] ;
         }
-        return x  ;
+        int ans = arrsum - valsum ;
+        System.out.println(ans) ;
+
+
+
     }
 }
