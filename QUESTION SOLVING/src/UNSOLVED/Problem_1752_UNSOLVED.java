17a71d47c1952e990d12d04b634e873117547f5e
MYMISERABLEMIND17 <tomarparakram17@gmail.com>
2026-01-10 16:46:31 +0530

    finally  complete problem 121

--- QUESTION SOLVING/src/UNSOLVED/Problem_1752_UNSOLVED.java
+++ QUESTION SOLVING/src/UNSOLVED/Problem_1752_UNSOLVED.java
@@ -1,28 +1,18 @@
 package UNSOLVED;
 
+import java.util.Arrays;
+
 public class Problem_1752_UNSOLVED {
     public static void main(String[] args) {
         int[] arr = {3, 4, 5, 1, 2};
         logic(arr) ;
     }
 
-    static int logic(int[] arr) {
+    static void logic(int[] arr) {
+        for (int i = 0  ; i< arr.length ; i++ ){
 
 
-        for (int i = 0; i < arr.length ; i++) {
-            int j = i+1 ;
-            int drop = 0 ;
-            if(arr[i] < arr[j] ){
-                drop++ ;
-            } else if (drop > 1) {
-
-            }
         }
 
-
-
-
-
-        return 0 ;
     }
 }
