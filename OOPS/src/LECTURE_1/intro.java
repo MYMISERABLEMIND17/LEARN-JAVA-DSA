baa0e73f3c480eac526809e3e8f79e2384399f5b
MYMISERABLEMIND17 <tomarparakram17@gmail.com>
2026-03-03 17:12:21 +0530

    learning about the constructor

--- OOPS/src/LECTURE_1/intro.java
+++ OOPS/src/LECTURE_1/intro.java
@@ -1,23 +1,37 @@
 package LECTURE_1;
 
+import java.util.Arrays;
+
 public class intro {
     public static void main(String[] args) {
-        System.out.println("hello world ");
+//        Student[] student = new Student[5];
+//        System.out.println(Arrays.toString(student));
 
-        int[] rno = new  int[5] ;
-        String[] name  = new String[5] ;
-        float[] marks = new float[5] ;
+        Student kunal = new Student(22,"this is the 2 argument one  ");
+        System.out.println(kunal.rno);
+        System.out.println(kunal.x);
 
-//        Student[] students = new Student[5] ;
+        Student om  = new Student(33," this is the  argument one" , 55) ;
+        System.out.println(om.marks);
+        System.out.println(om.rno);
+        System.out.println(om.x);
     }
-
-    class Student {
-        int[] rno = new  int[5] ;
-        String[] name  = new String[5] ;
-        float[] marks = new float[5] ;
-    }
-
-
-
-
 }
+    class Student {
+        int rno ;
+        String x ;
+        float marks ;
+
+        Student(int rno ,String x ){
+            this.rno = rno ;
+            this.x = x ;
+        }
+
+        Student(int rno ,String x , float marks){
+            this.rno = rno ;
+            this.x = x ;
+            this.marks = marks ;
+        }
+    }
+
+
