d60fc645de21d81038cfe88e40ac930f3950ef5f
MYMISERABLEMIND17 <tomarparakram17@gmail.com>
2026-03-11 14:56:11 +0530

    done 219

---
+++ QUESTION SOLVING/src/Problem_219.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class Problem_219 {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,1};
+        int k = 3 ;
+
+        System.out.println(logic(arr , k ));
+    }
+    static boolean logic( int[] nums , int k ){
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length ; i++) {
+            for (int j = i+1; j < nums.length; j++) {
+                if(nums[i] == nums[j] && j-i <=k  ){
+                    return true ;
+                }
+                if(j > i+ k ){
+                    break ;
+                }
+            }
+        }
+        return false ;
+    }
+}
---
+++ QUESTION SOLVING/src/UNSOLVED/Problem_11.java
@@ -0,0 +1,20 @@
+//public class Problem_11 {
+//    public static void main(String[] args) {
+//        int[] arr = {1,8,6,2,5,4,8,3,7} ;
+//    }
+//    static void logic(int[] height){
+//        int j = 0 ;
+//        int maxwidth = 0  ;
+//        int maxheight = 0 ;
+//        for (int i = 0; i < height.length ;  i++) {
+//            int width = height[i]  - height[j] ;
+//
+//            if(height[i] < ){
+//
+//            }
+//
+//
+//        }
+//
+//    }
+//}
