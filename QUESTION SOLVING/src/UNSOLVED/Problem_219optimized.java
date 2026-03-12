3e39e31bfd7c31e4af02521d30c7c7d5c8e60026
MYMISERABLEMIND17 <tomarparakram17@gmail.com>
2026-03-12 10:04:08 +0530

    solve the problem 344

---
+++ QUESTION SOLVING/src/UNSOLVED/Problem_219optimized.java
@@ -0,0 +1,19 @@
+package UNSOLVED;
+
+public class Problem_219optimized {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,1} ;
+        int k = 3 ;
+
+        System.out.println(logic(arr , k ));
+    }
+    static boolean logic(int[]nums , int k ){
+        for (int i = 0; i < nums.length-1; i++) {
+            if(nums[i]  == nums[i+1]){
+                return true ;
+            }
+        }
+        return false ;
+
+    }
+}
