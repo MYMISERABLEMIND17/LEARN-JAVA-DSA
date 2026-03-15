d95a8380f9a309c6ec72fbde1f8b2a3b261bc876
MYMISERABLEMIND17 <tomarparakram17@gmail.com>
2026-03-15 11:44:03 +0530

    complete the problem 11

---
+++ QUESTION SOLVING/src/Problem_11.java
@@ -0,0 +1,34 @@
+public class Problem_11 {
+    public static void main(String[] args) {
+        int[] arr = {1,1} ;
+        logic(arr);
+    }
+    static void logic(int[] height) {
+        int width = height.length ;
+        int start  = 0 ;
+        int end  = height.length-1 ;
+        int maxarea  = -1 ;
+        while(start < end ) {
+            int heightmin = 0 ;
+
+            if(height[start] > height[end]){
+                heightmin = height[end] ;
+                end -- ;
+                width-- ;
+            }
+            else {
+                heightmin = height[start] ;
+                start++ ;
+                width-- ;
+            }
+            int area  = width * heightmin  ;
+            if(area > maxarea){
+                maxarea = area ;
+            }
+        }
+        System.out.println(maxarea);
+
+
+
+    }
+}
