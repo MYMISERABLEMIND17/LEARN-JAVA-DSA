69853cc3bf2edf8e89cea4ad479b64841807c22f
MYMISERABLEMIND17 <tomarparakram17@gmail.com>
2026-03-24 19:41:43 +0530

    solved the problem 1614

---
+++ QUESTION SOLVING/src/Problem_1614.java
@@ -0,0 +1,24 @@
+public class Problem_1614 {
+    public static void main(String[] args) {
+        String s = "(()(()))" ;
+        logic(s);
+    }
+    static void logic (String str){
+        int depth  = 0 ;
+        int temp = 0  ;
+        int i = 0 ;
+        while ( i < str.length()){
+            if(str.charAt(i) == '('){
+                temp++ ;
+                if(temp > depth){
+                    depth = temp ;
+                }
+            }
+            else if (str.charAt(i) == ')' && temp > 0 ) {
+                temp--  ;
+            }
+            i++ ;
+        }
+        System.out.println(depth);
+    }
+}
