7b75e25a18a28cbb7942b81bd5c40b52c4ab71cd
MYMISERABLEMIND17 <tomarparakram17@gmail.com>
2026-03-26 10:26:09 +0530

    solved the problem 3110

---
+++ QUESTION SOLVING/src/Problem_3110.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class Problem_3110 {
+    public static void main(String[] args) {
+        String s = "hello" ;
+        logic(s);
+
+    }
+    static void logic(String str){
+        int[] arr = new int[str.length()] ;
+        int  i = 0 ;
+        while( i < str.length()){
+            char c  = str.charAt(i) ;
+            int ascii = c ;
+            arr[i] = ascii ;
+            i++ ;
+        }
+        System.out.println(Arrays.toString(arr));
+
+        int total = 0 ;
+        int j = 0 ;
+        while ( j < arr.length-1 ){
+            int calc = arr[j] - arr[j+1];
+            if( calc < 0 ){
+                calc = calc * (-1   ) ;
+            }
+            total = total + calc ;
+            j++;
+        }
+        System.out.println(total);
+    }
+}
