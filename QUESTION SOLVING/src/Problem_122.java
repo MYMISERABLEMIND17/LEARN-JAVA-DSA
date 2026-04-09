49200e4f827d6f068b9b3ccc2f966d08fb6bfa1d
MYMISERABLEMIND17 <tomarparakram17@gmail.com>
2026-04-09 19:50:56 +0530

    complete problem 122

---
+++ QUESTION SOLVING/src/Problem_122.java
@@ -0,0 +1,23 @@
+public class Problem_122 {
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 2, 4, 1, 5} ;
+        logic(arr);
+    }
+    static void logic (int[] nums ){
+        int i = 0 ;
+        int j = 0  ;
+        int profit = 0  ;
+        while( i < nums.length){
+            if(nums[i] > nums[j] ){
+                profit = profit +(nums[i] - nums[j]) ;
+                j = i ;
+            }
+            else if( nums[i] < nums[j]) {
+                j = i ;
+
+            }
+            i++ ;
+        }
+        System.out.println(profit);
+    }
+}
