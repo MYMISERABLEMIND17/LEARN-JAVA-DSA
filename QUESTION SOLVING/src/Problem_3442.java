1a19def97e3b79a859bee78f211a0eae79321a00
MYMISERABLEMIND17 <tomarparakram17@gmail.com>
2026-07-11 11:24:35 +0530

    complete the question 3442

---
+++ QUESTION SOLVING/src/Problem_3442.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+
+public class Problem_3442 {
+    public static void main(String[] args){
+        String s = "mmsmsym";
+        System.out.println(logic(s));
+    }
+
+    static int logic(String str){
+        char[] arr = str.toCharArray();
+        Arrays.sort(arr);
+
+        int maxOdd = 0;
+        int minEven = Integer.MAX_VALUE;
+        int freq = 1;
+
+        for (int i = 1; i <= arr.length; i++) {
+            if (i < arr.length && arr[i] == arr[i - 1]) {
+                freq++;
+            } else {
+                // a group just closed
+                if (freq % 2 == 0) {
+                    if (freq < minEven) {
+                        minEven = freq;
+                    }
+                } else {
+                    if (freq > maxOdd) {
+                        maxOdd = freq;
+                    }
+                }
+                freq = 1;
+            }
+        }
+
+        return maxOdd - minEven;
+    }
+}
