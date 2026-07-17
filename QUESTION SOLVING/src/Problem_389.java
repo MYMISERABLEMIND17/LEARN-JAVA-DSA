07f9f8a38e80f2019b0ccccd0fb16c6d0e9d8ef8
MYMISERABLEMIND17 <tomarparakram17@gmail.com>
2026-07-17 10:40:22 +0530

    solve the leetcode 389

---
+++ QUESTION SOLVING/src/Problem_389.java
@@ -0,0 +1,21 @@
+import static java.lang.Character.isLowerCase;
+import static java.lang.Character.toLowerCase;
+
+public class Problem_389 {
+    public static void main(String[] args ){
+        String s = "abcd" ;
+        String t = "abcde" ;
+        logic(s , t ) ;
+    }
+    static void logic(String f ,String s  ){
+        String n = s+f ;
+        StringBuilder x = new StringBuilder(n) ;
+        char a  = ' ' ;
+        for(int i = 0 ; i < x.length() ; i++ ){
+//            System.out.println(x.charAt(i)) ;
+            a = (char) (a ^ x.charAt(i));
+        }
+        char g = (char) (a+ 32) ;
+        System.out.println(g ) ;
+    }
+}
