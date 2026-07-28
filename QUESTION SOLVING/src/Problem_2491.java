9b5dc0c68de4f20bcacea32f15832cb24c07b0d9
MYMISERABLEMIND17 <tomarparakram17@gmail.com>
2026-07-28 14:34:23 +0530

    solved the problem 2491

--- QUESTION SOLVING/src/Problem_2491.java
+++ QUESTION SOLVING/src/Problem_2491.java
@@ -1,5 +1,25 @@
-public class Problem_2491 {
-    public static void main(String[] args) {
+import java.util.Arrays;
 
+public class Problem_2491 {
+    public static void main(String[] args){
+        int[] skill = {3,4} ;
+        Arrays.sort(skill) ;
+        int i =  0 ;
+        int j  = skill.length-1 ;
+        int currsum = skill[i] + skill[j] ;
+        long total = 0 ;
+        while(i < j ){
+            if((skill[i] + skill[j]) == currsum){
+                total = ( total + (skill[i] * skill[j])) ;
+
+            }
+            else{
+                System.out.println("-1") ;
+            }
+            i++ ;
+            j-- ;
+        }
+
+        System.out.println(total) ;
     }
 }
